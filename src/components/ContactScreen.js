de425c297410b67c3b2dec8cbe98fbe0372e703a
apaz-cli <aarpazdera@gmail.com>
2021-02-18 09:07:27 -0600

    Added ContactScreen component.

---
+++ src/components/ContactScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import ReactHtmlParser from 'react-html-parser';
+import {AuthConsumer} from './AuthContext'
+import { chatsRef } from '../firebase';
+  
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+class ContactScreen extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentUser: '',
+            contacts: []
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    render() {
+        return (
+        <AuthConsumer>
+        {({user}) => (
+            
+        )}
+        </AuthConsumer>
+        )
+    }
+}
+
+export default ContactScreen;
