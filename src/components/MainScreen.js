55b6fab20bef34043e26c7c1ba40481910676df6
AzureAD\BakkenTanner <bakkent2039@my.uwstout.edu>
2021-02-23 12:10:52 -0600

    Updated styling, and working on the componentDidMount feature

--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -17,6 +17,23 @@ class MainScreen extends React.Component {
     //    (e) => fetchChats()
     //}
 
+    componentDidMount(){
+        <AuthConsumer>
+            {({fetchChats, userChats, goToChat}) => (
+                <>
+                    fetchChats();
+                    
+                    {Object.keys(userChats).map(key =>
+                        <div key={key}>
+                            <button className="border-black border-2 bg-yellow-500" onClick={(e) => goToChat(userChats[key])}>Go to Chat: {userChats[key]}</button>
+                        </div>
+                        )}
+                </>
+
+            )}
+        </AuthConsumer>
+
+    }
 
     
     render() {
@@ -25,8 +42,8 @@ class MainScreen extends React.Component {
         <AuthConsumer>
             {({signUp, logIn, user, authMessage, logOut, createChat, fetchChats, userChats, goToChat, chatBot, goToProfile}) => (
              <>
-             <div className='storyScreen bg-gray-300 h-48'>
-             <div className="storyHeader flex flex-col h-48 w-full bg-gray-300 font-mono py-16">
+             <div className='storyScreen bg-gray-300 h-32'>
+             <div className="storyHeader flex flex-col h-32 w-full bg-gray-300 font-mono py-4">
                     <p className="lg:text-5xl md:text-3xl sm-text-xl break-words text-center">Stories</p>
                     <span className="FormHeader text-center text-black lg:text-4xl md:text-2xl sm:text-xl font-mono">
                         <button className="border-black border-2 bg-yellow-500 " onClick={(e)=> <p>"Create Story"</p>}>Create Story</button> </span>
@@ -34,22 +51,26 @@ class MainScreen extends React.Component {
                     </div>
                     
                         <div className='mainScreen bg-gray-500 h-screen'>
-                    <div className="mainScreenHeader flex flex-col h-1/3 w-full bg-gray-500 font-mono py-16">
+                    <div className="mainScreenHeader flex flex-col h-1/3 w-full bg-gray-500 font-mono py-4">
                     <p className="block lg:text-9xl md:text-6xl sm-text-5xl break-words text-center">Main Menu</p>
-                    </div>
+                    
                     <span className="FormHeader block text-center text-black lg:text-4xl md:text-2xl sm:text-xl font-mono">
-                        
-                    <button className="border-black border-2 bg-yellow-500" onClick={(e)=> createChat("N8kGtE5uqcgQVfHycKUuTuSU1uI3")}>Create Chat</button>
-                        {Object.keys(userChats).map(key =>
+                    <button className="border-black border-2 bg-yellow-500" onClick={(e)=> createChat("N8kGtE5uqcgQVfHycKUuTuSU1uI3")}>New Chat</button>
+                    <button className="border-black border-2 bg-yellow-500 " onClick={(e) => fetchChats()}>Get Chats</button>
+                    <button className="border-black border-2 bg-yellow-500 " onClick={(e)=> goToProfile()}>Settings</button> 
+                    <button className="border-black border-2 bg-yellow-500 " onClick={(e)=> chatBot()}>Chat Bot</button> 
+                    <button className="border-black border-2 bg-yellow-500 " onClick={(e)=> logOut()}>Log Out</button> </span>
+                    <br/>
+                    <p className="block lg:text-7xl md:text-4xl sm-text-3xl break-words text-left">Chats</p>
+                    <br/>
+                    <span className="FormHeader block text-left text-black lg:text-4xl md:text-2xl sm:text-xl font-mono">
+                    {Object.keys(userChats).map(key =>
                         <div key={key}>
                             <button className="border-black border-2 bg-yellow-500" onClick={(e) => goToChat(userChats[key])}>Go to Chat: {userChats[key]}</button>
                         </div>
                         )}
-                    <button className="border-black border-2 bg-yellow-500 " onClick={(e) => fetchChats()}>Fetch Chats</button>
-                    <button className="border-black border-2 bg-yellow-500 " onClick={(e)=> goToProfile()}>Profile Settings</button> 
-                    <button className="border-black border-2 bg-yellow-500 " onClick={(e)=> chatBot()}>Chat Bot</button> 
-                    <button className="border-black border-2 bg-yellow-500 " onClick={(e)=> logOut()}>Log Out</button> </span>
-                    <span text-center>Will update button appearance eventually, and get it to fetch the chats automatically</span>
+                        </span>
+                    </div>
                     </div>
             </>
             )}
