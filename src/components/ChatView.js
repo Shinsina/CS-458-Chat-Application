7250c4c97000be0695f3678f529e395a94845f08
shinsina <shinsina@att.net>
2021-02-25 15:53:38 -0600

    Slowly adding schedule message view

--- src/components/ChatView.js
+++ src/components/ChatView.js
@@ -15,7 +15,9 @@ class ChatView extends React.Component {
         this.state = {
             chatId: '',
             content: '',
-            messages: []
+            messages: [],
+            schedulingMessage: false,
+            months: ['January','February','March','April','May','June','July','August','September','October','November', 'December']
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -67,6 +69,31 @@ class ChatView extends React.Component {
             console.log(error)
         }
     }
+
+    scheduleMessage = async () => {
+        try  {
+            let sent = false
+            while (!sent) {
+                let scheduledTime = prompt('When should this message be sent? (Date should be of form MM/DD/YYYY HH:MM:SS (Timezone)', Date().toString())
+                if (scheduledTime !== null) {
+                const parseDate = Date.parse(scheduledTime)
+                const setDate = new Date(parseDate)
+                console.log(setDate)
+                if (setDate == 'Invalid Date') {
+                    sent = false;
+                }
+                else {
+                    sent = true;
+                }
+            }
+            else {
+                sent = true;
+            }
+            }
+        } catch (error) {
+
+        }
+    }
       
     render () {
         return (
@@ -77,6 +104,31 @@ class ChatView extends React.Component {
             <div className="bg-gray-500 h-screen">
             <div className="h-2/3">
             <div className="h-14 text-center text-lg w-full bg-yellow-500"><p className="py-3">{this.state.chatId}</p></div>
+            {this.state.schedulingMessage === true ? (
+                <form className="h-full text-center">
+                    <select id="month">
+
+                    </select>
+                    <select id="day">
+
+                    </select>
+                    <select id="year">
+
+                    </select>
+                    <select id="hour">
+
+                    </select>
+                    <select id="minute">
+
+                    </select>
+                    <select id="second">
+
+                    </select>
+                    <select id="timezone">
+
+                    </select>
+                </form>
+            ) : (
             <div className="h-full overflow-y-scroll">
             {Object.keys(this.state.messages).map(key => 
                 
@@ -93,9 +145,10 @@ class ChatView extends React.Component {
                     
                 </div>
                 )}
-                </div>
+            </div>
+    )}
             <form onSubmit={(e) => this.handleSubmit(e,userInfo)}>
-            <span className="block text-center w-full bg-yellow-500 text-black"><button className="w-full" type="submit">Submit</button></span>
+            <span className="block text-center w-full bg-yellow-500 text-black"><button className="w-1/2" type="submit">Submit</button><button className="w-1/2" type="button" onClick={(e) => this.setState({schedulingMessage: true})}>Schedule This Message</button></span>
             <span className="block h-full"><Editor value={this.state.content} init={{resize: false, plugins: [
              'advlist autolink lists link image charmap print preview anchor',
              'searchreplace visualblocks code fullscreen',
